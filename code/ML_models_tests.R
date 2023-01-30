20fb43f8a2ca6f5491fd3f8ec0f50643e9ff4172
Tommaso Grandi <118896276+tommasograndi@users.noreply.github.com>
2023-01-30 18:07:46 +0100

    Update ML_models_tests.R

--- code/ML_models_tests.R
+++ code/ML_models_tests.R
@@ -8,3 +8,22 @@ prova = predict(model, df)
 prova = data.frame(prova)
 df_test_lm = data.frame(c(df, prova)) #creating df with predictions attached to original df
 df_test_lm = df_test_lm[, c(1,4,7,8,24,15)]
+
+
+
+
+
+
+
+#CLASSIFICATION (not yet runned, just written to be tested later)
+
+# train model
+#df.decision.train = rpart(formula, data=df_train, method="class")
+
+# predict probabilities of class
+#df.decision.test = predict(model.rpart, newdata=df_test, method="prob")
+
+# we want to predict the probability of class=1 (winner) or class=0 (not winner)
+#Then we are going to sort the probabilities and pick the greater probability of class=1, hence the driver with 
+#the greatest probability of being a winner. 
+
