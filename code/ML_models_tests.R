6501f0833595561bdac872ea45f55611b9538f6b
Riccardo <51230348+riccardo-borgo@users.noreply.github.com>
2023-01-31 15:49:46 +0100

    test with bayes

--- code/ML_models_tests.R
+++ code/ML_models_tests.R
@@ -9,6 +9,24 @@ prova = data.frame(prova)
 df_test_lm = data.frame(c(df, prova)) #creating df with predictions attached to original df
 df_test_lm = df_test_lm[, c(1,4,7,8,24,15)]
 
+# BAYES CLASSIFIER
+library(caret)
+library(e1071)
+for (i in 1:nrow(df.results.merged)) {
+  if (df.results.merged[i, "positionText"] == '1'){
+    df.results.merged[i, 'winner'] <- 1
+  }else{
+    df.results.merged[i, 'winner'] <- 0
+  }
+}
+df.idx <- sample(nrow(df), 19178)
+df.train <- df.results.merged[df.idx,]
+df.test <- df.results.merged[-df.idx,]
+
+df.nb <- naiveBayes(winner ~ ., data = df.results.merged)
+df.nb
+prediction <- predict(df.nb, df.test)
+table(df.test$winner, prediction)
 
 #CLASSIFICATION (not yet runned, just written to be tested later)
 
