96323b3adccdb99becf48a4373df991acfce57b0
Tommaso Grandi <118896276+tommasograndi@users.noreply.github.com>
2023-02-03 15:47:18 +0100

    updating models

--- code/ML_models_tests.R
+++ code/ML_models_tests.R
@@ -27,20 +27,21 @@ levels(df.test$name) <- levels(df.train$name)
 #linear regression
 linearmodel = lm(positionOrder ~ grid + number + laps + fastestLapSpeed + round + const_points + const_wins + driver_age + fastestLap_ms, data = df.train)
 predict_lm = predict(linearmodel, df.test)
-score_regression(df.test, predict_lm) #performed 54%
+score_regression(df.test, predict_lm) 
 summary(linearmodel)
 
 #REGRESSION TREE
-predict_rt = predict(tree(positionOrder ~ grid + number + laps + fastestLapSpeed + round + const_points + const_wins + driver_age + fastestLap_ms
+predict_rt = predict(tree(positionOrder ~ grid + number + laps + fastestLapSpeed + round + const_points + const_wins + driver_age + fastestLap_ms + wins
                                , df.train), newdata = df.test)
-score_regression(df.test, predict_rt) #34%
+score_regression(df.test, predict_rt) #47%
+
 
 ### KNN REGRESSION
-library(FNN)
-library(KernelKnn)
-knn_regressor = knnreg(positionOrder ~ grid + number + laps + fastestLapSpeed + round + const_points + const_wins + driver_age + fastestLap_ms, data = df.train, k = 15) 
-prediction_knn <- predict(knn_regressor, newdata = df.test, )
-score_regression(df.test, prediction_knn) #37%
+#library(FNN)
+#library(KernelKnn)
+#knn_regressor = knnreg(positionOrder ~ grid + number + laps + fastestLapSpeed + round + const_points + const_wins + driver_age + fastestLap_ms + wins, data = df.train, k = 10) 
+#prediction_knn <- predict(knn_regressor, newdata = df.test, )
+#score_regression(df.test, prediction_knn) #37%
 
 
 ### CLASSIFICATION TECHNIQUES
@@ -50,31 +51,38 @@ library(e1071)
 #Then we are going to sort the probabilities and pick the greater probability of class=1, hence the driver with 
 #the greatest probability of being a winner. 
 
+#We are removing categorical features with a lot of levels since they are not providing useful informations to the model. 
+
 # BAYES CLASSIFIER
-df.nb <- naiveBayes(winner ~ . -positionOrder -resultId -points, data = df.train)
+df.nb <- naiveBayes(winner ~ . -positionOrder -resultId -points -status -fullname -const_name, data = df.train)
 prediction_nb <- predict(df.nb, newdata = df.test, type = 'raw')
-score_classification(df.test, prediction_nb) #41%
+score_classification(df.test, prediction_nb) #50%
  
 #DECISION TREE.
 #using df.test 
-df.dt = rpart(winner ~ . -positionOrder -resultId -points, data = df.train, method="class")
+df.dt = rpart(winner ~ . -positionOrder -resultId -points -status -fullname -const_name, data = df.train, method="class")
 prediction_dt = predict(df.dt, newdata = df.test, method="prob")
-score_classification(df.test, prediction_dt) #50%
+score_classification(df.test, prediction_dt) #58%
+rpart.plot(df.dt,faclen = 2)
 
-#SVM CLASSIFICATION (using classic df.test)
-df.lsvm = svm(winner ~ . , data = df.train[, -c(8,9)], kernel = 'radial', fitted = FALSE, probability = TRUE)   #sigmoid kernel working best
-prediction_svm <- predict(df.lsvm, newdata = df.test[,-c(8,9)], fitted = FALSE, probability = TRUE)
+
+#SVM CLASSIFICATION.
+df.lsvm = svm(winner ~ . , data = df.train[, -c(5,8,9,12,15)], kernel = 'radial', fitted = FALSE, probability = TRUE)   #sigmoid kernel working best
+prediction_svm <- predict(df.lsvm, newdata = df.test[,-c(5,8,9,12,15)], fitted = FALSE, probability = TRUE)
 SVM_class = data.frame(attributes(prediction_svm)$probabilities)
-score_classification(df.test, SVM_class) #52%. (when removing factors was just 32%)
-#32% with linear, 50.7% with polynomial, 46% with sigmoid, 52% with radial basis. 
+score_classification(df.test, SVM_class) #56%
+#50% with linear, 48% with polynomial, 56% with sigmoid, 56% with radial basis. 
 
 #RANDOM FOREST
 df.rf <- randomForest(winner ~ . -points -positionOrder -resultId -const_name -name -fullname -status, data = df.train, ntree = 200)
 prediction.rf <- predict(df.rf, df.test, type = 'prob')
 prediction.rf[is.na(prediction.rf)] <- 0
-score_classification(df.test.clean, prediction.rf) #56,6%
+score_classification(df.test.clean, prediction.rf) #60%
 
 
+
+
+#####################
 ### TRYING CLASSIFICATION BY REMOVING CATEGORICAL FEATURE ISSUE using NAs substitution
 #we are using df.train.clean and df.test.clean (copies of df.train and df.clean but without categorical features converted in factors)
 #also, in df.test.clean we substituted categories not present in df.train.clean with NAS
@@ -84,22 +92,20 @@ df.test.clean$fullname[which(!(df.test.clean$fullname %in% unique(df.train.clean
 df.test.clean$const_name[which(!(df.test.clean$const_name %in% unique(df.train.clean$const_name)))] <- NA
 df.test.clean$name[which(!(df.test.clean$name %in% unique(df.train.clean$name)))] <- NA
 
-#DECISION TREE (+8%)
-df.dt.clean = rpart(winner ~ . -positionOrder -resultId -points, data = df.train.clean, method="class")
+#DECISION TREE
+df.dt.clean = rpart(winner ~ . -positionOrder -resultId -points -status, data = df.train.clean, method="class")
 prediction_dt_clean = predict(df.dt.clean, newdata = df.test.clean, method="prob")
-score_classification(df.test.clean, prediction_dt_clean) #58 (without this technique was 50%)
+score_classification(df.test.clean, prediction_dt_clean) #54% (without this technique was 50%)
+plot(df.dt.clean)
+text(df.dt.clean)
 
-#BAYES CLASSIFIER (+10%)
-df.nb <- naiveBayes(winner ~ . -positionOrder -resultId -points, data = df.train.clean)
-prediction_nb <- predict(df.nb, newdata = df.test.clean, type = 'raw')
-score_classification(df.test.clean, prediction_nb) #51%
 
-#RANDOM FOREST
-library(randomForest)
-df.rf <- randomForest(winner ~ . -points -positionOrder -resultId, data = df.train.clean, ntree = 200)
-prediction.rf <- predict(df.rf, df.test.clean, type = 'prob')
-prediction.rf[is.na(prediction.rf)] <- 0
-score_classification(df.test.clean, prediction.rf) #45%
+#BAYES CLASSIFIER (+8%)
+df.nb.clean <- naiveBayes(winner ~ . -positionOrder -resultId -points, data = df.train.clean)
+prediction_nb_clean <- predict(df.nb, newdata = df.test.clean, type = 'raw')
+score_classification(df.test.clean, prediction_nb_clean) #52%
+
+
 
 
 
